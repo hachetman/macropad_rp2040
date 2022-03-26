61444b05e85d456d1b6e573223dc51a088360a80
Christian Grumbein <christian@grumbein.de>
2022-03-26 06:47:05 +0100

    initial commit with CDC and HID USB endpoints

---
+++ src/sh1106.cc
@@ -0,0 +1,2 @@
+#include "sh1106.h"
+auto sh1106::init() -> uint32_t { return 0; }
---
+++ src/sh1106.h
@@ -0,0 +1,9 @@
+#ifndef SH1106_H_
+#define SH1106_H_
+#include "pico/stdlib.h"
+class sh1106 {
+public:
+  auto init() -> uint32_t;
+};
+
+#endif // SH1106_H_
